50681cabfd5246ef479333f14cbf57a7c0a4ab54
KnightWeb0 <webworkdev711@gmail.com>
2024-07-17 18:09:58 +0100

    first commit after setting up vuetify

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <!-- <RouterView /> -->
+  <VRow justify="center">
+    <VCol cols="3" class="mt-2">
+      <VCard width="250" height="350" flat variant="outlined" rounded="xl">
+        <VRow>
+          <VCol class="text-center">
+            <VCardTitle class="text-h5"> Oli Sykes</VCardTitle>
+            <VImg
+              rounded="lg"
+              class="mt-1 ml-6"
+              cover
+              width="200px"
+              src="https://upload.wikimedia.org/wikipedia/commons/b/b3/2016_RiP_Bring_Me_the_Horizon_-_Oliver_Sykes_-_by_2eight_-_8SC6713.jpg"
+            />
+            <VCardText class="pa-0 mt-1"> Role: Artist</VCardText>
+            <VCardText class="pa-0 mt-1"> Band:<strong> Bring Me The Horizon </strong> </VCardText>
+            <VCardText class="pa-0 mt-1">
+              Record label: <strong> Epitaph Records </strong>
+            </VCardText>
+          </VCol>
+        </VRow>
+      </VCard>
+    </VCol>
+  </VRow>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.use(vuetify)
+
+app.mount('#app')
